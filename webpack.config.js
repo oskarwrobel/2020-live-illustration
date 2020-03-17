432b63260ae65fe4489c006d9439de30fa68233d
Oskar Wróbel <kontakt@oskarwrobel.pl>
2020-03-17 16:21:22 +0100

    Init repository.

---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+'use strict';
+
+/* eslint-env node */
+
+const path = require( 'path' );
+const { DefinePlugin } = require( 'webpack' );
+const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+
+module.exports = ( env = {} ) => {
+	const projectDir = path.dir( __filename );
+
+	const main = [ path.join( projectDir, 'src', 'scripts', 'app.ts' ) ];
+
+	if ( env.analytics ) {
+		main.push( path.join( projectDir, 'src', 'scripts', 'analytics.js' ) );
+	}
+
+	return {
+		entry: { main },
+
+		output: {
+			filename: '[name].[contenthash].js',
+			path: path.join( process.cwd(), 'dist' ),
+			publicPath: '/'
+		},
+
+		module: {
+			rules: [
+				{
+					test: /\.ts(x?)$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: 'ts-loader'
+						}
+					]
+				},
+				// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+				{
+					enforce: 'pre',
+					test: /\.js$/,
+					loader: 'source-map-loader'
+				},
+				{
+					test: /\.css$/,
+					use: [
+						'style-loader',
+						{
+							loader: 'css-loader',
+							options: {
+								importLoaders: 1
+							}
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								map: false,
+								plugins: () => [
+									require( 'postcss-nested' )
+								]
+							}
+						}
+					]
+				},
+				{
+					test: /.(png|jpg|jpeg|gif|svg|woff|woff2|wav|mp3|m4a)$/,
+					use: 'file-loader'
+				},
+				{
+					test: /\.html$/,
+					use: {
+						loader: 'html-loader',
+						options: {
+							interpolate: true
+						}
+					}
+				}
+			]
+		},
+
+		plugins: [
+			new CleanWebpackPlugin(),
+			new HtmlWebpackPlugin( {
+				chunks: [ 'main' ],
+				template: path.join( projectDir, 'src', 'index.html' )
+			} ),
+			new MiniCssExtractPlugin( {
+				filename: '[name].[contenthash].css'
+			} ),
+			new DefinePlugin( {
+				ANALYTICS: JSON.stringify( env.analytics )
+			} )
+		]
+	};
+};
