29e9652a37f39c35407c058d2c2f4330b4b32784
Oskar Wróbel <kontakt@oskarwrobel.pl>
2020-03-21 16:28:10 +0100

    Made a first step to use device orientation for parallax.

--- src/scripts/utils/parallax.ts
+++ src/scripts/utils/parallax.ts
@@ -1,4 +1,4 @@
-import { throttle } from 'lodash-es';
+import { throttle, clamp } from 'lodash-es';
 
 type Item = {
 	element: Element;
@@ -12,13 +12,37 @@ type Config = {
 
 function createParallax( config: Config ): void {
 	let isAnimationInProgress = false;
+	let lastValue: number;
 
-	function mouseMoveHandler( evt: MouseEvent ): void {
+	document.addEventListener( 'mousemove', throttle( ( evt: MouseEvent ) => {
+		const sceneRect = config.scene.getBoundingClientRect();
+		const center = sceneRect.width / 2;
+		const value = ( ( ( evt.clientX - sceneRect.left ) * 100 ) / center ) - 100;
+
+		move( clamp( value, -100, 100 ) );
+	}, 50, { leading: true } ) );
+
+	// window.addEventListener( 'deviceorientation', throttle( ( evt: DeviceOrientationEvent ) => {
+	// 	move( evt.alpha * 100 / 180 );
+	// }, 0, { leading: true } ) );
+
+	// if ( DeviceOrientationEvent && typeof DeviceOrientationEvent.requestPermission === 'function' ) {
+	// 	DeviceOrientationEvent.requestPermission()
+	// 		.then( ( response: string ) => {
+	// 			if ( response === 'rejected' ) {
+	// 				document.addEventListener( 'click', askForPermission );
+	// 			}
+	// 		} )
+	// 		.catch( () => document.addEventListener( 'click', askForPermission ) );
+	// }
+
+	function move( value: number ): void {
+		if ( lastValue === value ) {
+			return;
+		}
+
+		lastValue = value;
 		requestAnimationFrame( () => {
-			const sceneRect = config.scene.getBoundingClientRect();
-			const center = sceneRect.width / 2;
-			const move = ( ( ( evt.clientX - sceneRect.left ) * 100 ) / center ) - 100;
-
 			if ( !isAnimationInProgress ) {
 				isAnimationInProgress = true;
 				config.scene.classList.add( 'animation-in-progress' );
@@ -35,12 +59,18 @@ function createParallax( config: Config ): void {
 				const element = item.element as HTMLElement;
 				const friction = item.friction;
 
-				element.style.transform = `translateX( ${ -move * friction }% )`;
+				element.style.transform = `translateX( ${ -value * friction }% )`;
 			}
 		} );
 	}
 
-	document.addEventListener( 'mousemove', throttle( mouseMoveHandler, 50, { leading: true } ) );
+	// function askForPermission(): void {
+	// 	DeviceOrientationEvent.requestPermission().then( response => {
+	// 		if ( response === 'granted' ) {
+	// 			window.removeEventListener( 'click', askForPermission );
+	// 		}
+	// 	} );
+	// }
 }
 
 export default function parallax( config: Config ): void {
