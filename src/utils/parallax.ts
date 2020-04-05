0d63a988f36d458b3571d00733497ba86ba70787
Oskar Wróbel <kontakt@oskarwrobel.pl>
2020-04-05 12:29:15 +0200

    Changed friction to depth.

--- src/utils/parallax.ts
+++ src/utils/parallax.ts
@@ -2,7 +2,7 @@ import { throttle, clamp } from 'lodash-es';
 
 type Item = {
 	element: Element;
-	friction: number;
+	depth: number;
 	revert?: boolean;
 }
 
@@ -13,19 +13,23 @@ type Config = {
 
 export default function parallax( config: Config ): () => void {
 	let lastValue: number;
+	let sceneRect = config.scene.getBoundingClientRect();
 
 	config.scene.classList.add( 'parallax' );
 
 	const throttledMouseMoveHandler = throttle( mouseMoveHandler, 50, { leading: true } );
+	const throttledResizeHandler = throttle( resizeHandler, 100 );
 
 	document.addEventListener( 'mousemove', throttledMouseMoveHandler );
+	document.addEventListener( 'resize', throttledResizeHandler );
 
 	function mouseMoveHandler( evt: MouseEvent ): void {
-		const sceneRect = config.scene.getBoundingClientRect();
-		const center = sceneRect.width / 2;
-		const value = ( ( ( evt.clientX - sceneRect.left ) * 100 ) / center ) - 100;
+		requestAnimationFrame( () => {
+			const center = sceneRect.width / 2;
+			const value = ( ( ( evt.clientX - sceneRect.left ) * 100 ) / center ) - 100;
 
-		move( clamp( value, -100, 100 ) );
+			move( clamp( value, -100, 100 ) );
+		} );
 	}
 
 	// window.addEventListener( 'deviceorientation', throttle( ( evt: DeviceOrientationEvent ) => {
@@ -51,16 +55,21 @@ export default function parallax( config: Config ): () => void {
 		requestAnimationFrame( () => {
 			for ( const item of config.items ) {
 				const element = item.element as HTMLElement;
-				const friction = item.friction;
 
-				let x = -value * friction;
+				let x = -value * item.depth;
 
 				if ( item.revert ) {
 					x = -x;
 				}
 
-				element.style.transform = `translateX( ${ x }% )`;
+				element.style.transform = `translateX( ${ x }px )`;
 			}
+		} );
+	}
+
+	function resizeHandler(): void {
+		window.addEventListener( 'resize', () => {
+			sceneRect = config.scene.getBoundingClientRect();
 		} );
 	}
 
@@ -74,6 +83,7 @@ export default function parallax( config: Config ): () => void {
 
 	return function destroy(): void {
 		document.removeEventListener( 'mousemove', throttledMouseMoveHandler );
+		window.removeEventListener( 'resize', throttledResizeHandler );
 		config.scene.classList.remove( 'parallax' );
 	};
 }
